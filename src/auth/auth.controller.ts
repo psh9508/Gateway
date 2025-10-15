f00a4402303bf8a0861c8b72a9963258e0091651
masuri <psh0258@gmail.com>
2025-10-15 13:56:50 +0900

    refactor: improve code formatting and consistency in login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,26 +3,26 @@ import * as auth from './models/auth';
 
 @Controller('auth')
 export class AuthController {
-    @Post('login')
-    async login(@Body() request: auth.LoginReq) {
-        if(!request.login_id || !request.password) {
-            throw new BadRequestException('Login ID and password are required');
-        }
-
-        const url = 'http://localhost:8000/user/login'
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(request),
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        return responseData;
+  @Post('login')
+  async login(@Body() request: auth.LoginReq) {
+    if(!request.login_id || !request.password) {
+      throw new BadRequestException('Login ID and password are required');
     }
+
+    const url = 'http://localhost:8000/user/login';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  }
 }
