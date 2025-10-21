0505626332015428bd8a66799d2f3824cd3cee1a
masuri <psh0258@gmail.com>
2025-10-21 14:49:03 +0900

    feat: implement basic middleware code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { HealthCheckModule } from './health-check/health-check.module';
+import { RatelimitMiddleware } from './middleware/ratelimit.middieware';
 
 @Module({
   imports: [HealthCheckModule],
   controllers: [AuthController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RatelimitMiddleware)
+      .forRoutes('*');
+  }
+}
