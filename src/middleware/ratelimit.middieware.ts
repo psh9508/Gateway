e1cbf075815dc8ad00a9adfe82915fd70e4b900f
masuri <psh0258@gmail.com>
2025-10-22 23:28:52 +0900

    feat: enhance RatelimitMiddleware to include request ID logging

--- src/middleware/ratelimit.middieware.ts
+++ src/middleware/ratelimit.middieware.ts
@@ -1,10 +1,13 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class RatelimitMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        console.log('Request...');
+        const requestId = randomUUID();
+        res.setHeader('X-Request-Id', requestId)
+        console.log(`[${req.method}] ${req.originalUrl} | requestId: ${requestId}`);
         next();
     }
 }
