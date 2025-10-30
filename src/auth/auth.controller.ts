635f04331632a1788867a44a311cfbdf4276ac59
masuri <psh0258@gmail.com>
2025-10-30 20:28:52 +0900

    feat: auth server healthcheck endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,30 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import * as auth from './models/auth';
 import { config } from '../../config/config.loader';
 
 @Controller('auth')
 export class AuthController {
+  private readonly authEndpoint = config.get('server_endpoints').auth;
+
+  @Get('healthcheck')
+  async healthcheck(): Promise<boolean> {
+    const url = `http://${this.authEndpoint}/healthcheck`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return response.ok
+  }
+
   @Post('login')
   async login(@Body() request: auth.LoginReq) {
     if(!request.login_id || !request.password) {
       throw new BadRequestException('Login ID and password are required');
     }
-
-    const authEndpoint = config.get('server_endpoints').auth;
-    const url = `http://${authEndpoint}/user/login`;
+    const url = `http://${this.authEndpoint}/user/login`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
