6fc61c713d4b38cf8a03edf3c2b798af1fd4f040
masuri <psh0258@gmail.com>
2025-11-08 23:31:27 +0900

    feat: enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://d33my9j9pm3mqa.cloudfront.net'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
