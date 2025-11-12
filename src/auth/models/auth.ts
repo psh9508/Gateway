c203d98bb4a7c74b604ce20f5146f6932e0fec91
masuri <psh0258@gmail.com>
2025-11-13 00:49:23 +0900

    feat: add EmailVerificationReq interface for email verification process

--- src/auth/models/auth.ts
+++ src/auth/models/auth.ts
@@ -2,3 +2,9 @@ export interface LoginReq {
   login_id: string;
   password: string;
 }
+
+export interface EmailVerificationReq {
+  user_id: string;
+  login_id: string;
+  verification_code: string;
+}
